33ea73ba26acfdfc63ab86b25c3ea0ea770db88c
kirikiri <killian.podhajsky@gmail.com>
2023-07-01 18:16:04 -0700

    button trouble

--- assets/testcode.js
+++ assets/testcode.js
@@ -1,63 +1,51 @@
 const myQuestions = [
   {
     question: "What year was the original `The Texas Chainsaw Massacre` released?",
-    answers: {
-      a: "2003",
-      b: "1980",
-      c: "1974"
-    },
-    correctAnswer: "c"
+    choices: [ "2003", "1980","1974"
+    ],
+    answer: "1974"
   },
   {
     question: "Who directed `Halloween`?",
-    answers: {
-      a: "John Carpenter",
-      b: "Sam Rami",
-      c: "George Romero"
-    },
-    correctAnswer: "a"
+    choices: [
+      "John Carpenter", "Sam Rami", "George Romero"
+    ],
+    answer: "John Carpenter"
   },
   {
     choices: "What movie is the following quote from? `They're coming to get you, Barbara.",
-    answers: {
-      a: "Shaun of the Dead",
-      b: "Dawn of the Dead",
-      c: "Night of the Living Dead"
-    },
-    correctAnswer: "c"
+    choices: [ "Shaun of the Dead", "Dawn of the Dead","Night of the Living Dead"
+  ],
+    answer: "Night of the Living Dead"
   },
   {
     question: "How many movies are in the Saw franchise?",
-    choices: {
-      a: "10",
-      b: "6",
-      c: "9"
-    },
-    correctAnswer: "a"
+    choices: [
+       "10",
+       "6",
+       "9"
+    ],
+    answer: "10"
   },
   {
     question: "Where is The Overlook located in The Shining?",
-    answers: {
-      a: "Maine",
-      b: "New York",
-      c: "Colorado"
-    },
-    correctAnswer: "c"
+    choices: ["Maine","New York","Colorado"
+  ],
+    answer: "Colorado"
   },
   {
     question: "Who is the villian in Psycho?",
-    answers: {
-      a: "Norman Bateman",
-      b: "Norman Bates",
-      c: "Patrick Bates"
-    },
-    correctAnswer: "b"
+    choices: [
+       "Norman Bateman","Norman Bates","Patrick Bates"
+      ],
+    answer: "Norman Bates"
   },
 ];
 
 const startButton = document.querySelector("#start-button");
 const submitButton = document.querySelector("#submit-button");
-const answersElement = document.querySelector("#answers");
+const nextButton = document.querySelector("#next-button");
+const choicesElement = document.querySelector("#choices");
 const quizContainer = document.querySelector("#quiz-container");
 const timerElement = document.querySelector("#timer-count");
 const recordScore = document.querySelector("#record-score");
@@ -77,34 +65,38 @@ function startQuiz() {
   quizContainer.classList.remove("hide");
   timerId = setInterval(startTimer, 1000);
   timerElement.textContent = timerCount;
-  displayQuestions();
+  displayQuestion();
 }
 
-function displayQuestions() {
+function displayQuestion() {
   const question = myQuestions[questionIndex];
   document.querySelector("#question-title").textContent = question.question;
-  const answers = question.answers
-  .map(
-    (answer, i) => `<button class="answer" value="${answer}" onclick= "questionClick(this)">
-    ${i + 1}. ${answer}
-    </button>`
-  )
-  .join(" ");
-  answersElement.innerHTML = answers;
+  const choices = question.choices
+    .map(
+      (choice, i) => `
+      <label> <input type="radio" class="choice" value="${choice}">
+      ${i + 1}. ${choice}
+    </label>
+  `
+    )
+    .join("");
+  choicesElement.innerHTML = choices;
 }
 
-function questionClick(button) {
- const correctAnswer = button.value;
- if (correctAnswer !==quiz[questionIndex].correctAnswer){
-  timerCount = Math.max(timerCount -5, 0);
- }
- questionIndex ++;
- if (questionIndex === quiz.length) {
-  quizEnd();
- } else {
-  displayQuestions();
- }
+function questionClick(radio) {
+  const answer = radio.value;
+  if (answer !== myQuestions[questionIndex].answer) {
+    timerCount = Math.max(timerCount - 5, 0); // subtract 5 seconds for wrong answers
+  }
+  questionIndex++;
+  if (questionIndex === myQuestions.length) {
+    quizEnd();
+  } else {
+    displayQuestion();
+  }
 }
+
+
 
 function quizEnd() {
   clearInterval(timerId);
@@ -123,7 +115,7 @@ function startTimer() {
 function scoreRecord() {
   const initials = initialsElement.value.trim();
   if (!initials) return;
-  const highscore = { initials, score: timeLeft};
+  const highscore = { initials, score: timeCounter};
   highscores.push(highscore);
 
   highscores.sort((a, b) => b.score - a.score);
@@ -150,12 +142,4 @@ function showHighscores() {
 
 startButton.addEventListener("click", startQuiz);
 submitButton.addEventListener("click", quizEnd);
-
-
-  
-
-
-
-
-
-
+nextButton.addEventListener("click", displayQuestion);
